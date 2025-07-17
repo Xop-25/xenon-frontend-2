a72cbe871104fe4bed8d7d88be0b399e54e90b5a
Xop-25 <sachinshahi21145@gmail.com>
2025-07-17 15:17:02 +1000

    Create App.jsx

---
+++ src/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>XENON Platform is Alive</h1>
+      <p>Welcome to the beginning of recursive reality.</p>
+    </div>
+  );
+}
+
+export default App;
